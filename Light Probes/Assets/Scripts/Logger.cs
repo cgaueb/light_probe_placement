35e6d7b06e7fb169021ca3d38f9d9bb84bde62be
Kostas Vardis <kvardis@hotmail.com>
2021-01-11 21:12:49 +0200

    Added lock mechanism on logger, to accomodate async based decimation

--- Light Probes/Assets/Scripts/Logger.cs
+++ Light Probes/Assets/Scripts/Logger.cs
@@ -1,28 +1,50 @@
 ﻿using System;
+using System.Threading;
 
 public class LumiLogger
 {
+    private static Mutex mut = new Mutex();
     private static LumiLogger logger = new LumiLogger();
+    private string name = "log.txt";
+
     public static LumiLogger Logger {
         get { return logger; }
     }
 
     public void Log(String msg) {
+        mut.WaitOne();
+        string log_msg = DateTime.Now + " [" + Thread.CurrentThread.ManagedThreadId.ToString("00") + "]: Log [INFO]: " + msg;
         if (System.Diagnostics.Debugger.IsAttached) {
-            System.Diagnostics.Debug.Write("Log [INFO]: " + msg);
+            System.Diagnostics.Debug.Write(log_msg);
+        }
+        using (System.IO.StreamWriter file = new System.IO.StreamWriter(name, true)) {
+            file.WriteLine(log_msg);
         }
         UnityEngine.Debug.Log(msg);
+        mut.ReleaseMutex();
     }
     public void LogWarning(String msg) {
+        mut.WaitOne();
+        string log_msg = DateTime.Now + " [" + Thread.CurrentThread.ManagedThreadId.ToString("00") + "]: Log [WARN]: " + msg;
         if (System.Diagnostics.Debugger.IsAttached) {
-            System.Diagnostics.Debug.Write("Log [WARN]: " + msg);
+            System.Diagnostics.Debug.Write(log_msg);
+        }
+        using (System.IO.StreamWriter file = new System.IO.StreamWriter(name, true)) {
+            file.WriteLine(log_msg);
         }
         UnityEngine.Debug.LogWarning(msg);
+        mut.ReleaseMutex();
     }
     public void LogError(String msg) {
+        mut.WaitOne();
+        string log_msg = DateTime.Now + " [" + Thread.CurrentThread.ManagedThreadId.ToString("00") + "]: Log [ERRO]: " + msg;
         if (System.Diagnostics.Debugger.IsAttached) {
-            System.Diagnostics.Debug.Write("Log [ERRO]: " + msg);
+            System.Diagnostics.Debug.Write(log_msg);
+        }
+        using (System.IO.StreamWriter file = new System.IO.StreamWriter(name, true)) {
+            file.WriteLine(log_msg);
         }
         UnityEngine.Debug.LogError(msg);
+        mut.ReleaseMutex();
     }
 }
