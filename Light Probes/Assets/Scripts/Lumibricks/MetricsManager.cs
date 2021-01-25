ee97ef290cc66e3dd5ce3d7a452a08c26dc6ecf1
Kostas Vardis <kvardis@hotmail.com>
2021-01-25 19:48:28 +0200

    Redesigned luminance metric. Optimisation TODO: only compute the luminance value

--- Light Probes/Assets/Scripts/Lumibricks/MetricsManager.cs
+++ Light Probes/Assets/Scripts/Lumibricks/MetricsManager.cs
@@ -17,7 +17,7 @@ class MetricsManager
         public ColorTypeManager.ColorType colorType;
         public Vector3 weight;
 
-        public Vector3 WeightedMetric(Color value, Color reference) {
+        public virtual Vector3 WeightedMetric(Color value, Color reference) {
             Color converted_value = colorType.convertColor(value);
             Color converted_reference = colorType.convertColor(reference);
             return new Vector3(
@@ -41,10 +41,35 @@ class MetricsManager
         }
     }
 
+    class MetricLuminance : Metric
+    {
+        public override Vector3 WeightedMetric(Color value, Color reference) {
+            Color converted_value = colorType.convertColor(value);
+            Color converted_reference = colorType.convertColor(reference);
+            Vector3 res = new Vector3((converted_value.r - converted_reference.r) * weight.x, 0.0f, 0.0f);
+            res.y = res.x;
+            res.z = res.x;
+            return res;
+        }
+        public override Vector3[] WeightedEvaluation(Color value, Color reference) {
+            // TODO: only compute the luminance value
+            Color converted_value = colorType.convertColor(value);
+            Color converted_reference = colorType.convertColor(reference);
+            Vector3[] res = new Vector3[2];
+            res[0] = new Vector3(converted_value.r * weight.x, 0.0f, 0.0f);
+            res[0].y = res[0].x;
+            res[0].z = res[0].x;
+            res[1] = new Vector3(converted_reference.r * weight.x, 0.0f, 0.0f);
+            res[1].y = res[1].x;
+            res[1].z = res[1].x;
+            return res;
+        }
+    }
+
         Dictionary<MetricType, Metric> LightEvaluationMetricsList = new Dictionary<MetricType, Metric> {
         { MetricType.RGB, new Metric() },
         { MetricType.Chrominance, new Metric() },
-        { MetricType.Luminance, new Metric() }
+        { MetricType.Luminance, new MetricLuminance() }
     };
 
     public MetricType CurrentMetricType { get; private set; }
@@ -55,7 +80,7 @@ class MetricsManager
         // init metrics
         LightEvaluationMetricsList[MetricType.RGB].weight = new Vector3(0.33f, 0.33f, 0.33f);
         LightEvaluationMetricsList[MetricType.Chrominance].weight = new Vector3(0.1f, 0.45f, 0.45f);
-        LightEvaluationMetricsList[MetricType.Luminance].weight = new Vector3(0.5f, 0.25f, 0.25f);
+        LightEvaluationMetricsList[MetricType.Luminance].weight = new Vector3(1.0f, 0.0f, 0.0f);
         LightEvaluationMetricsList[MetricType.RGB].colorType = new ColorTypeManager.RGBType();
         LightEvaluationMetricsList[MetricType.Chrominance].colorType = new ColorTypeManager.YCoCgType();
         LightEvaluationMetricsList[MetricType.Luminance].colorType = new ColorTypeManager.YCoCgType();
